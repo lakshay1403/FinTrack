92d0dffab071026814ab7bd77dc08ea7eb19cb19
lakshay1403 <rudrakshg16@gmail.com>
2026-06-10 18:02:24 +0530

    added a error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const userRoute = require('./routes/userRoute');
+const errorHandler = require('./middlewares/errorhandler');
 
 
 const connectDB = async () => {
@@ -22,7 +23,10 @@ connectDB();
 app.use(express.json());  //pass incoming json data
 //!Routes
 app.use('/', userRoute);
+//Error
+app.use(errorHandler);
 const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
