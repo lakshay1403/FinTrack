a744bc30c599a989a23c74e2ae27271754673a0a
lakshay1403 <rudrakshg16@gmail.com>
2026-06-20 14:57:37 +0530

    added a controller in transaction for updating and deleting the transactions and testing the api in postman

--- backend/routes/transactionRoute.js
+++ backend/routes/transactionRoute.js
@@ -6,5 +6,7 @@ const transactionRoute = express.Router();
 
 transactionRoute.post('/api/v1/transactions/create', isAuthenticated, TransactionController.create);
 transactionRoute.get('/api/v1/transactions/lists', isAuthenticated, TransactionController.getFilteredTransactions);
+transactionRoute.put("/api/v1/transactions/update/:id", isAuthenticated, TransactionController.update);
+transactionRoute.delete("/api/v1/transactions/delete/:id", isAuthenticated, TransactionController.delete);
 
 module.exports = transactionRoute;
