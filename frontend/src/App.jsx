b4dfdba01cf7e7f17b7de2b85ef4d1b7980900c6
lakshay1403 <rudrakshg16@gmail.com>
2026-06-21 15:14:10 +0530

    generated a few templateds from ai and routed in app.jsx for register and login with navbar and hero-section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HeroSection from "../Components/Home/HomePage"
+import PublicNavbar from "../Components/Navbar/PublicNavbar";
+import LoginForm from "../Components/Users/Login";
+import RegistrationForm from "../Components/Users/Register";
 
 function App() {
   return (
-    <>
-      <h1c className ="text-3xl font-bold underline">Hello world!</h1c>
-    </>
-  )
+    <BrowserRouter>
+    <PublicNavbar/>
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/Login" element = {<LoginForm/>} />
+        <Route path="/register" element = {<RegistrationForm/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
